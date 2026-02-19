bbe1e030904565a9ece0e25d761b78659dc8377a
mohammadreza <mohammadreza.mohammadii1996@gmail.com>
2026-02-19 15:26:01 -0800

    Update dataset.py

--- src/model/src/dataset.py
+++ src/model/src/dataset.py
@@ -1,1 +1,21 @@
+import torch
+from torch.utils.data import Dataset
+from PIL import Image
+import os
+from torchvision import transforms
 
+class SRDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.files = os.listdir(root_dir)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.root_dir, self.files[idx])
+        image = Image.open(img_path).convert('RGB')
+        if self.transform:
+            image = self.transform(image)
+        return image
