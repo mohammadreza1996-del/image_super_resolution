1f20a49d23101f6ef169f9d8a3858c11f7da1e82
mohammadreza <mohammadreza.mohammadii1996@gmail.com>
2026-02-19 15:27:08 -0800

    Update train.py

--- src/model/src/train.py
+++ src/model/src/train.py
@@ -1,21 +1,37 @@
 import torch
-from torch.utils.data import Dataset
-from PIL import Image
-import os
+from torch.utils.data import DataLoader
 from torchvision import transforms
+from src.model import SRCNN
+from src.dataset import SRDataset
+import torch.nn as nn
+import torch.optim as optim
 
-class SRDataset(Dataset):
-    def __init__(self, root_dir, transform=None):
-        self.root_dir = root_dir
-        self.files = os.listdir(root_dir)
-        self.transform = transform
+# device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    def __len__(self):
-        return len(self.files)
+# Dataset و Dataloader
+transform = transforms.Compose([
+    transforms.ToTensor(),
+])
+train_dataset = SRDataset(root_dir='data/train', transform=transform)
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
 
-    def __getitem__(self, idx):
-        img_path = os.path.join(self.root_dir, self.files[idx])
-        image = Image.open(img_path).convert('RGB')
-        if self.transform:
-            image = self.transform(image)
-        return image
+
+model = SRCNN().to(device)
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=1e-4)
+
+
+num_epochs = 50
+for epoch in range(num_epochs):
+    for imgs in train_loader:
+        imgs = imgs.to(device)
+        outputs = model(imgs)
+        loss = criterion(outputs, imgs)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}")
+
+
+torch.save(model.state_dict(), 'models/srcnn.pth')
