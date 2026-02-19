be66145b9a97d7e3e8005849f666567b8e32793a
mohammadreza <mohammadreza.mohammadii1996@gmail.com>
2026-02-19 15:05:51 -0800

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,17 @@
+import torch
+import torch.nn as nn
 
+class SRCNN(nn.Module):
+    def __init__(self):
+        super(SRCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=9, padding=4)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(64, 32, kernel_size=5, padding=2)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.conv3 = nn.Conv2d(32, 3, kernel_size=5, padding=2)
+
+    def forward(self, x):
+        x = self.relu1(self.conv1(x))
+        x = self.relu2(self.conv2(x))
+        x = self.conv3(x)
+        return x
