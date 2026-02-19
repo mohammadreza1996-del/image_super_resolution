431216fe7f20c449d5f22196664b02bbb83523c2
mohammadreza <mohammadreza.mohammadii1996@gmail.com>
2026-02-19 15:27:39 -0800

    Update utils.py

--- src/model/src/utils.py
+++ src/model/src/utils.py
@@ -1,1 +1,10 @@
+import matplotlib.pyplot as plt
+import torchvision
 
+def show_images(lr, sr):
+    fig, axs = plt.subplots(1, 2)
+    axs[0].imshow(lr.permute(1,2,0))
+    axs[0].set_title("Low-Res")
+    axs[1].imshow(sr.permute(1,2,0))
+    axs[1].set_title("Super-Resolved")
+    plt.show()
